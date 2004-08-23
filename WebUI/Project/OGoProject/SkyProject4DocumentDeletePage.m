70589998eca64c4afc7f1c43d1d85450ae9841c2
Helge Heß <me@helgehess.eu>
2004-08-23 15:45:22 +0000

    fixed some linking pathes
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@48 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- WebUI/Project/OGoProject/SkyProject4DocumentDeletePage.m
+++ WebUI/Project/OGoProject/SkyProject4DocumentDeletePage.m
@@ -1,7 +1,7 @@
 /*
-  Copyright (C) 2000-2003 SKYRIX Software AG
+  Copyright (C) 2000-2004 SKYRIX Software AG
 
-  This file is part of OGo
+  This file is part of OpenGroupware.org.
 
   OGo is free software; you can redistribute it and/or modify it under
   the terms of the GNU Lesser General Public License as published by the
@@ -18,7 +18,6 @@
   Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
   02111-1307, USA.
 */
-// $Id$
 
 #include <OGoFoundation/LSWViewerPage.h>
 
@@ -29,11 +28,9 @@
 
 @implementation SkyProject4DocumentDeletePage
 
-- (id)activateObject:(id)_obj
-                verb:(NSString *)_verb
-                type:(NGMimeType *)_type {
+- (id)activateObject:(id)_obj verb:(NSString *)_verb type:(NGMimeType *)_type {
   id fm;
-
+  
   fm = [(SkyProjectDocument *)_obj fileManager];
   [fm trashFileAtPath:[_obj path] handler:nil];
   
