5d0bd351d2bceabd31b8e1b8cbb1f868450d36b8
Helge Heß <me@helgehess.eu>
2004-09-13 20:02:40 +0000

    improvements
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@130 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- Contrib/svn-mv-wo-themes.sh
+++ Contrib/svn-mv-wo-themes.sh
@@ -5,7 +5,11 @@
 for COMP in `find . -type d -name "*.wo"`; do
   echo "check $COMP ..."
   LPROJS="`ls -d $COMP/*.lproj 2>/dev/null`"
-  for LPROJ in $LPROJS; do
-    echo "  check language `basename $LPROJ` ..."
+  for LPROJPATH in $LPROJS; do
+    LPROJ=`basename $LPROJPATH`
+    LANG=`echo $LPROJ | sed s/.lproj// | sed s#_#/#`
+    LANGNAME="`dirname $LANG`"
+    THEME="`basename $LANG`"
+    echo "  check language $LANGNAME theme $THEME ..."
   done
 done
