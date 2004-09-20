fdf565679462f16b290216453a347444dba5a43b
Helge Heß <me@helgehess.eu>
2004-09-20 19:51:22 +0000

    added support for ChangeLog processing
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@156 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- Contrib/compare-releases.py
+++ Contrib/compare-releases.py
@@ -50,7 +50,7 @@ def shouldHideFile(filename):
     if filename in excludeList:
         return 1
     for s in excludeSuffixes:
-        if filename[-len(s):] == s:
+        if filename.endswith(s):
             return 1
     return 0
 
@@ -66,13 +66,34 @@ def makeRelPath(base, path):
     #print "relpath:", relpath
     return relpath
 
+def compareChangeLogs(pathA, pathB):
+    if os.stat(pathA)[stat.ST_SIZE] == os.stat(pathB)[stat.ST_SIZE]:
+        "sufficiently correct in practice, ChangeLogs should always grow ..."
+        return None
+    sA = open(pathA, "r").read()
+    sB = open(pathB, "r").read()
+    if len(sA) > len(sB):
+        t = sA
+        sA = sB
+        sB = t
+    t = sB[:-len(sA)]
+    del sA, sB
+    return t
+
 def compareDirectories(pathA, pathB, recurse=0):
     contentsA = os.listdir(pathA)
     contentsB = os.listdir(pathB)
     same, added, removed = compareLists(contentsA, contentsB)
     result = { 'added': [], 'removed': [], 'children': {} }
     
+    bothHaveChangeLog = "ChangeLog" in contentsA and "ChangeLog" in contentsB
+    if bothHaveChangeLog:
+        txt = compareChangeLogs(pathA + "/ChangeLog", pathB + "/ChangeLog")
+        if txt is not None:
+            result['ChangeLog'] = txt
+    
     relpath = makeRelPath(treeA, pathA)
+    result['path'] = relpath
     
     for p in added:
         result['added'].append(relpath + p)
@@ -95,21 +116,33 @@ def compareDirectories(pathA, pathB, recurse=0):
 
 # end comparePathes
 
-def printDifferences(diffs):
+def printChangeLog(ChangeLog, indent, compress=0):
+    for line in ChangeLog.splitlines(0):
+        if compress:
+            stripped = line.strip()
+            if len(stripped) == 0:
+                continue
+        print indent, line
+
+def printDifferences(diffs, compress=0):
+    if diffs.has_key("ChangeLog"):
+        print "changed:", diffs['path']
+        printChangeLog(diffs['ChangeLog'], "    ", compress)
+    
     if diffs.has_key("added"):
         for p in diffs['added']:
             print "added:  ", p
     if diffs.has_key("removed"):
         for p in diffs['removed']:
             print "removed:", p
+    
     if diffs.has_key("children"):
         for p, subdiffs in diffs['children'].items():
-            printDifferences(subdiffs)
+            printDifferences(subdiffs, compress)
 
 # run
 
 print "compare", treeA, "with", treeB
 
-result = compareDirectories(treeA, treeB, 1)
-printDifferences(result)
-
+result = compareDirectories(treeA, treeB, recurse=1)
+printDifferences(result, compress=1)
