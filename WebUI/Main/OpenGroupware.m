d233fc7a3a1d074af94debdfe8246d6ef7b90165
Helge Heß <me@helgehess.eu>
2004-09-27 19:21:33 +0000

    fixed an OSX gcc issue
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@205 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- WebUI/Main/OpenGroupware.m
+++ WebUI/Main/OpenGroupware.m
@@ -171,11 +171,11 @@ static NSString *FHSOGoBundleDir = @"lib/opengroupware.org-1.0a/";
 
 - (NSString *)name {
   /* override to avoid clashes with "." processing */
-  static NSString *name = nil;
-  if (name != nil)
-    return name;
-  name = [[[[NSProcessInfo processInfo] arguments] objectAtIndex:0] copy];
-  return name;
+  static NSString *cName = nil;
+  if (cName != nil)
+    return cName;
+  cName = [[[[NSProcessInfo processInfo] arguments] objectAtIndex:0] copy];
+  return cName;
 }
 
 - (void)loadBundlesOfType:(NSString *)_type inPath:(NSString *)_p {
