ba90754d3c317f6a1551725ec5002f04f532f5fc
Sebastian Ley <ley@debian.org>
2004-10-20 11:31:16 +0000

    Remove autoinstall for skyaptnofify for now. This has to be tweaked for the new instances system
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@269 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- debian/opengroupware.org-skyaptnotify.skyaptnotify.cron.d
+++
@@ -1,7 +0,0 @@
-# Instruct cron to call skyaptnotify in regular intervals. Skyaptnotify sends
-# out appointment notifications for OpenGroupware.org via mail.
-
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-*/5 * * * * ogo [ -x /usr/lib/opengroupware.org/System/Tools/skyaptnotify ] && . /usr/lib/opengroupware.org/System/Makefiles/GNUstep.sh && /usr/lib/opengroupware.org/System/Tools/skyaptnotify >> /var/log/opengroupware.org-skyaptnotify.log 2>&1
