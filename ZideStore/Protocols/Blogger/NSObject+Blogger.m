d48b4a63d3a3c73d5d8bfa0f336d37b79a8a5870
Helge Heß <me@helgehess.eu>
2004-10-24 21:26:27 +0000

    check NSFileSubject for post title
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@295 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- ZideStore/Protocols/Blogger/NSObject+Blogger.m
+++ ZideStore/Protocols/Blogger/NSObject+Blogger.m
@@ -63,79 +63,6 @@ static NSNumber *noNum = nil;
   return infos;
 }
 
-- (NSString *)bloggerContentInContext:(id)_ctx {
-  NSString *str;
-  
-  if ((str = [self valueForKey:@"contentAsString"]) == nil)
-    return nil;
-  
-  if ([str hasPrefix:OGo_HTML_MARKER])
-    return [str substringFromIndex:[OGo_HTML_MARKER length]];
-
-  str = [NSString stringWithFormat:@"<p>%@</p>", 
-		    [str stringByEscapingHTMLString]];
-  return str;
-}
-
-- (NSString *)bloggerPostIDInContext:(id)_ctx {
-  /* default post ID is container name + entry name */
-  NSString *tmp;
-  
-  tmp = [[self container] nameInContainer];
-  tmp = [tmp stringByAppendingString:@"/"];
-  return [tmp stringByAppendingString:[self nameInContainer]];
-}
-- (NSDictionary *)bloggerPostInfoInContext:(id)_ctx {
-  /*
-    Returns array of structs containing 
-      dateCreated
-      userid
-      postid
-      description
-      title
-      link
-      permaLink
-      mt_excerpt
-      mt_text_more
-      mt_allow_comments
-      mt_allow_pings
-      mt_convert_breaks
-      mt_keywords
-  */
-  NSMutableDictionary *entry;
-  NSString *str;
-  id tmp;
-
-  if (noNum == nil) noNum = [[NSNumber numberWithBool:NO] retain];
-  
-  entry = [NSMutableDictionary dictionaryWithCapacity:24];
-  
-  [entry setObject:[self bloggerPostIDInContext:_ctx] forKey:@"postid"];
-  if ((tmp = [self baseURLInContext:_ctx])) {
-    [entry setObject:tmp forKey:@"link"];
-    [entry setObject:tmp forKey:@"permaLink"];
-  }
-  
-  /* description is the first text */
-  if ((str = [self bloggerContentInContext:_ctx]) != nil)
-    [entry setObject:str forKey:@"description"];
-  
-  /* there can be a second text in 'mt_text_more' */
-  
-  if ((tmp = [self davLastModified]))
-    [entry setObject:tmp forKey:@"dateCreated"];
-  if ((tmp = [self davDisplayName]))
-    [entry setObject:tmp forKey:@"title"];
-  
-  [entry setObject:noNum forKey:@"mt_allow_comments"];
-  [entry setObject:noNum forKey:@"mt_allow_pings"];
-#if 0 /* This is a _string_ argument (the key of mt.supportedTextFilters) */
-  [entry setObject:myFilter forKey:@"mt_convert_breaks"];
-#endif
-  
-  return entry;
-}
-
 - (id)lookupBlogWithID:(NSString *)_blogID inContext:(id)_ctx {
   [self logWithFormat:@"lookup BLOG: %@", _blogID];
   return [self lookupName:_blogID inContext:_ctx acquire:NO];
@@ -198,3 +125,84 @@ static NSNumber *noNum = nil;
 }
 
 @end /* NSObject(BlogObject) */
+
+@implementation NSObject(PostObject)
+
+- (NSString *)bloggerPostIDInContext:(id)_ctx {
+  /* default post ID is container name + entry name */
+  NSString *tmp;
+  
+  tmp = [[self container] nameInContainer];
+  tmp = [tmp stringByAppendingString:@"/"];
+  return [tmp stringByAppendingString:[self nameInContainer]];
+}
+
+- (NSString *)bloggerContentInContext:(id)_ctx {
+  NSString *str;
+  
+  if ((str = [self valueForKey:@"contentAsString"]) == nil)
+    return nil;
+  
+  if ([str hasPrefix:OGo_HTML_MARKER])
+    return [str substringFromIndex:[OGo_HTML_MARKER length]];
+
+  str = [NSString stringWithFormat:@"<p>%@</p>", 
+		    [str stringByEscapingHTMLString]];
+  return str;
+}
+
+- (NSDictionary *)bloggerPostInfoInContext:(id)_ctx {
+  /*
+    Returns a structs containing 
+      dateCreated
+      userid
+      postid
+      description
+      title
+      link
+      permaLink
+      mt_excerpt
+      mt_text_more
+      mt_allow_comments
+      mt_allow_pings
+      mt_convert_breaks
+      mt_keywords
+  */
+  NSMutableDictionary *entry;
+  NSString *str;
+  id tmp;
+
+  if (noNum == nil) noNum = [[NSNumber numberWithBool:NO] retain];
+  
+  entry = [NSMutableDictionary dictionaryWithCapacity:24];
+  
+  [entry setObject:[self bloggerPostIDInContext:_ctx] forKey:@"postid"];
+  if ((tmp = [self baseURLInContext:_ctx])) {
+    [entry setObject:tmp forKey:@"link"];
+    [entry setObject:tmp forKey:@"permaLink"];
+  }
+  
+  /* description is the first text */
+  if ((str = [self bloggerContentInContext:_ctx]) != nil)
+    [entry setObject:str forKey:@"description"];
+  
+  /* there can be a second text in 'mt_text_more' */
+  
+  if ((tmp = [self davLastModified]))
+    [entry setObject:tmp forKey:@"dateCreated"];
+  
+  if ([(tmp = [self valueForKey:@"NSFileSubject"]) isNotNull])
+    [entry setObject:tmp forKey:@"title"];
+  else if ((tmp = [self davDisplayName]))
+    [entry setObject:tmp forKey:@"title"];
+  
+  [entry setObject:noNum forKey:@"mt_allow_comments"];
+  [entry setObject:noNum forKey:@"mt_allow_pings"];
+#if 0 /* This is a _string_ argument (the key of mt.supportedTextFilters) */
+  [entry setObject:myFilter forKey:@"mt_convert_breaks"];
+#endif
+  
+  return entry;
+}
+
+@end /* NSObject(PostObject) */
