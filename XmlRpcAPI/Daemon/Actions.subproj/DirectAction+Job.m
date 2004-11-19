e4bc55870f5cb832077db377557ac1fb9678cd27
Helge Heß <me@helgehess.eu>
2004-11-19 01:51:57 +0000

    fixed an Id
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@333 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- XmlRpcAPI/Daemon/Actions.subproj/DirectAction+Job.m
+++ XmlRpcAPI/Daemon/Actions.subproj/DirectAction+Job.m
@@ -18,7 +18,6 @@
   Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
   02111-1307, USA.
 */
-// $Id: DirectAction+Job.m 1 2004-08-20 11:17:52Z znek $
 
 #include "DirectAction.h"
 #include <EOControl/EOControl.h>
