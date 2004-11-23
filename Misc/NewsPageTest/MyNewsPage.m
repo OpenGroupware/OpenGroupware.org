c52fc3f1113f8f625305cf9780d78d9ec517b0cf
Helge Heß <me@helgehess.eu>
2004-11-23 21:59:30 +0000

    added example for news page extension more fixes for FHS in HelloWorld
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@346 fda58e92-02e2-0310-b1e4-b2b32738dd10

---
+++ Misc/NewsPageTest/MyNewsPage.m
@@ -0,0 +1,24 @@
+
+#include <OGoFoundation/OGoComponent.h>
+
+/*
+  MyNewsPage
+  
+  This is our page component. It inherits from OGoComponent mostly to gain the
+  label translation system.
+  
+  To make the page appear in the news application, we declare it as a
+  'NewsPage' in the bundle-info.plist.
+*/
+
+@interface MyNewsPage : OGoComponent
+{     
+}
+@end 
+
+#import <Foundation/Foundation.h>
+#include <OGoFoundation/OGoSession.h>
+
+@implementation MyNewsPage
+
+@end /* MyNewsPage */
