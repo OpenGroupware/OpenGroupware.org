4d02263bc04b655e3edf6e1b9fb34de76354a291
Helge Heß <me@helgehess.eu>
2004-12-06 19:59:09 +0000

    added missing list renderers
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@441 fda58e92-02e2-0310-b1e4-b2b32738dd10

---
+++ ZideStore/SoObjects/ZSContacts/ZSPersonListEntryRenderer.m
@@ -0,0 +1,92 @@
+/*
+  Copyright (C) 2004 Helge Hess
+
+  This file is part of OpenGroupware.org.
+
+  OGo is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 2, or (at your option) any
+  later version.
+
+  OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with OGo; see the file COPYING.  If not, write to the
+  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
+
+#include "ZSPersonListEntryRenderer.h"
+#include "common.h"
+
+@implementation ZSPersonListEntryRenderer
+
+static id sharedRenderer = nil; // THREAD
+
++ (id)sharedListEntryRenderer {
+  if (sharedRenderer == nil)
+    sharedRenderer = [[self alloc] init];
+  return sharedRenderer;
+}
+
+- (void)dealloc {
+  [super dealloc];
+}
+
+/* rendering */
+
+- (id)renderEntry:(id)_entry representingSoObject:(id)_object {
+  // contentlength,lastmodified,displayname,executable,resourcetype
+  // checked-in,checked-out
+  /*
+    <key name="{DAV:}href"    >$baseURL$/$pkey$.vcf?sn=$sn$</key>
+    <key name="davContentType">text/vcard</key>
+    <key name="davDisplayName">$sn$, $givenname$</key>
+  */
+  NSMutableDictionary *record;
+  NSString *url, *dname;
+  NSString *sn, *pkey;
+  id tmp;
+  
+  if ((record = [[_entry mutableCopy] autorelease]) == nil)
+    return nil;
+
+  // [self logWithFormat:@"RENDER: %@", record];
+  
+  // getting: pkey, sn, givenname
+  sn   = [record objectForKey:@"sn"];
+  pkey = [[record objectForKey:@"pkey"] stringValue];
+  
+  /* get URL */
+  
+  // TODO: do not use formats
+  url = [NSString stringWithFormat:@"%@%@.vcf", [_object baseURL], pkey];
+  if ([sn length] > 0)
+    url = [url stringByAppendingFormat:@"?sn=%@", [sn stringByEscapingURL]];
+  
+  [record setObject:url forKey:@"{DAV:}href"];
+  
+  /* render display name */
+  
+  tmp   = [record objectForKey:@"givenname"];
+  dname = [sn isNotNull]
+    ? [[sn stringByAppendingString:@", "] stringByAppendingString:tmp]
+    : nil;
+  if (dname != nil)
+    [record setObject:dname forKey:@"davDisplayName"];
+
+  /* render etag */
+  
+  if ([(tmp = [record objectForKey:@"version"]) isNotNull]) {
+    tmp = [@":" stringByAppendingString:[tmp stringValue]];
+    tmp = [pkey stringByAppendingString:tmp];
+    [record setObject:tmp forKey:@"davEntityTag"];
+  }
+  
+  return record;
+}
+
+@end /* ZSPersonListEntryRenderer */
