f0b53242dd35b6ffbedb04f4f38fb129f9f47827
Frank Reppin <frank@opengroupware.org>
2005-01-23 12:27:20 +0000

    move mod_ngobjweb package into OGo repo
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@674 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- maintenance/rpm_buildenvironment/trunk_apt4rpm_build.pl
+++ maintenance/rpm_buildenvironment/trunk_apt4rpm_build.pl
@@ -74,8 +74,8 @@ my @apt_items = `/bin/ls -laA /var/virtual_hosts/download/packages/$distri/$type
 foreach $item (@apt_items) {
   chomp $item;
   push @tp_rpms, $item if ($item =~ m/^libfoundation.*$|^libobjc.*$|^ogo-gnustep.*$|^epoz.*$/i);
-  push @ogo_rpms, $item if (($item =~ m/^ogo-.*$/i) and ($item !~ m/^ogo-gnustep_make.*/i));
-  push @sope_rpms, $item if ($item =~ m/^sope.*$|^libical-sope.*$|^mod_ngobjweb.*$/i);
+  push @ogo_rpms, $item if (($item =~ m/^ogo-.*$|^mod_ngobjweb.*$/i) and ($item !~ m/^ogo-gnustep_make.*/i));
+  push @sope_rpms, $item if ($item =~ m/^sope.*$|^libical-sope.*$/i);
 }
 
 if ("$verbose" eq "yes") {
