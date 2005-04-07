b411cd2120a89e3bd367536979b14bde36997f9f
Frank Reppin <frank@opengroupware.org>
2005-04-07 09:17:59 +0000

    create and sign 'Release' file (Bug #1183)
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@925 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- maintenance/deb_buildenvironment/trunk_debian_apt.sh
+++ maintenance/deb_buildenvironment/trunk_debian_apt.sh
@@ -3,17 +3,39 @@
 
 FLAVOUR="$1"
 
+
 if [ "x${FLAVOUR}" = "x" ]; then
   echo -en "No flavour given...\n"
   echo -en "choose either 'sid' or 'sarge'\n"
   exit 1
 fi
 
+if [ ! -e "${HOME}/sign_rpm_passphrase.secret" ]; then
+  echo -e "No secret found..."
+  echo -e "cannot proceed!"
+  exit 0
+fi
+
+source ${HOME}/sign_rpm_passphrase.secret
+
+echo "Archive: download.opengroupware.org
+Version: 1.0+opengroupware.org
+Component: ${FLAVOUR}/trunk
+Origin: OpenGroupware.org
+Label: OpenGroupware.org Debian packages
+Architecture: i386" >/var/virtual_hosts/download/packages/debian/dists/${FLAVOUR}/trunk/binary-i386/Release
+
+/usr/bin/expect -c \
+  "spawn /usr/bin/gpg --sign /var/virtual_hosts/download/packages/debian/dists/${FLAVOUR}/trunk/binary-i386/Release; \
+   expect \"Enter pass phrase:\"; \
+   send -- \"${PASSPHRASE}\\r\"; \
+   expect eof" >/dev/null
+
 rm -fr ${HOME}/tmp/*
 mkdir -p ${HOME}/tmp/
 mv /var/virtual_hosts/download/packages/debian/dists/${FLAVOUR}/trunk/binary-i386/*-latest* ${HOME}/tmp/
 cd /var/virtual_hosts/download/packages/debian/dists/${FLAVOUR}/trunk
-dpkg-scanpackages binary-i386 /dev/null dists/${FLAVOUR}/trunk/ > binary-i386/Packages
+/usr/bin/dpkg-scanpackages binary-i386 /dev/null dists/${FLAVOUR}/trunk/ > binary-i386/Packages
 cd /var/virtual_hosts/download/packages/debian/dists/${FLAVOUR}/trunk/binary-i386/
 gzip -c Packages > Packages.gz
 mv ${HOME}/tmp/* /var/virtual_hosts/download/packages/debian/dists/${FLAVOUR}/trunk/binary-i386/
