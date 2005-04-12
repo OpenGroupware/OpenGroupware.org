76d6bbb936eb6fdb30d71f007e6bcacc3a40d5ed
Frank Reppin <frank@opengroupware.org>
2005-04-12 13:49:02 +0000

    fix typo
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@938 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- maintenance/rpm_buildenvironment/release_apt4rpm_build.pl
+++ maintenance/rpm_buildenvironment/release_apt4rpm_build.pl
@@ -20,6 +20,7 @@ my @distris = qw( fedora-core3
   suse82
   suse91
   suse92
+  suse93
   mdk-10.1
   mdk-10.0
   slss8
@@ -27,7 +28,6 @@ my @distris = qw( fedora-core3
   rhel3
   redhat9
   conectiva10
-  suse93
 );
 
 getopt('dtvn');
@@ -115,7 +115,7 @@ print RELEASE "Version: $type\n";
 print RELEASE "Origin: http://download.opengroupware.org\n";
 print RELEASE "Label: $distri\n";
 print RELEASE "Architecture: i386\n" if (("$distri" eq "suse82") or ("$distri" eq "fedora-core2") or ("$distri" eq "fedora-core3") or ("$distri" eq "rhel3") or ("$distri" eq "redhat9") or ("$distri" eq "conectiva10"));
-print RELEASE "Architecture: i586\n" if (("$distri" eq "suse91") or ("$distri" eq "suse92") or ("$distri" eq "sles9") or ("$distri" eq "mdk-10.0") or ("$distri" eq "mdk-10.1") ("$distri" eq "suse93"));
+print RELEASE "Architecture: i586\n" if (("$distri" eq "suse91") or ("$distri" eq "suse92") or ("$distri" eq "suse93") or("$distri" eq "sles9") or ("$distri" eq "mdk-10.0") or ("$distri" eq "mdk-10.1"));
 print RELEASE "NotAutomatic: false\n";
 close(RELEASE);
 
