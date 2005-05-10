2260d930a7ce3bc636b5f52c93f1ea3be84eb828
Helge Heß <me@helgehess.eu>
2005-05-10 19:29:54 +0000

    minor cleanups
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@985 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- Migration/SLEMS3/import/SkyPersonImport.m
+++ Migration/SLEMS3/import/SkyPersonImport.m
@@ -1,23 +1,45 @@
+/*
+  Copyright (C) 2002-2005 SKYRIX Software AG
 
-#include "common.h"
+  This file is part of OpenGroupware.org.
+
+  OGo is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 2, or (at your option) any
+  later version.
+
+  OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with OGo; see the file COPYING.  If not, write to the
+  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
+
 #include "SkyPersonImport.h"
 #include "SkyPersonUidHandler.h"
 #include "SkyGroupUidHandler.h"
+#include "common.h"
 #include <LSFoundation/LSFoundation.h>
 
 @implementation SkyPersonImport
 
 - (id)initWithPersonsPath:(NSString *)_path {
   if ((self = [super init])) {
-    ASSIGN(self->path, _path);
+    self->path = [_path copy];
   }
   return self;
 }
 
 - (void)dealloc {
-  RELEASE(self->path);
+  [self->path release];
   [super dealloc];
 }
+
+/* operations */
 
 - (NSArray *)objects {
   NSArray        *array;
@@ -25,15 +47,17 @@
   id             obj;
   NSMutableArray *result;
 
-  array = [self->fm directoryContentsAtPath:self->path];
+  array  = [self->fm directoryContentsAtPath:self->path];
   result = [NSMutableArray arrayWithCapacity:[array count]];
   enumerator = [array objectEnumerator];
-  while ((obj = [enumerator nextObject])) {
+  while ((obj = [enumerator nextObject]) != nil) {
     NSMutableDictionary *dict;
-    dict = [NSMutableDictionary dictionaryWithContentsOfFile:
+    
+    dict = [[NSMutableDictionary alloc] initWithContentsOfFile:
                                 [path stringByAppendingPathComponent:obj]];
     [dict setObject:[obj stringByDeletingPathExtension] forKey:@"uid"];
     [result addObject:dict];
+    [dict release];
   }
   return result;
 }
@@ -59,30 +83,29 @@
   
   NSEnumerator *enumerator;
 
-  if (AddrType == nil)
+  if (AddrType == nil) {
     AddrType = [[NSArray alloc]
                       initWithObjects:@"location", @"mailing",
                       @"private", nil];
-  if (AddrAttr == nil)
+  }
+  if (AddrAttr == nil) {
     AddrAttr = [[NSArray alloc]
                       initWithObjects:@"name1", @"name2", @"name3",
                          @"country", @"state", @"street", @"zip", @"city",
                          nil];
-  
-  if (TelType == nil)
+  }
+  if (TelType == nil) {
     TelType = [[NSArray alloc]
                       initWithObjects:
-                         @"01_tel",
-                         @"03_tel_funk",
-                         @"05_tel_private",
+                         @"01_tel", @"03_tel_funk", @"05_tel_private",
                          @"10_fax",nil];
-
-    
-  ctx = [self commandContext];
-
-  if (![_obj isKindOfClass:[NSMutableDictionary class]]) {
-    _obj = [[_obj mutableCopy] autorelease];
   }
+  
+  ctx = [self commandContext];
+  
+  if (![_obj isKindOfClass:[NSMutableDictionary class]])
+    _obj = [[_obj mutableCopy] autorelease];
+  
   [_obj setObject:[NSNumber numberWithInt:_id] forKey:@"companyId"];
 
   {
@@ -93,7 +116,7 @@
       [_obj setObject:[NSNumber numberWithBool:YES] forKey:@"isPrivate"];
     }
   }
-
+  
   obj = [ctx runCommand:@"person::new" arguments:_obj];
 
   {
@@ -102,8 +125,8 @@
 
     addrs      = [NSMutableDictionary dictionaryWithCapacity:3];
     enumerator = [[obj valueForKey:@"toAddress"] objectEnumerator];
-
-    while ((o = [enumerator nextObject])) {
+    
+    while ((o = [enumerator nextObject]) != nil) {
       [addrs setObject:[o valueForKey:@"addressId"]
              forKey:[o valueForKey:@"type"]];
     }
@@ -115,7 +138,7 @@
     tels       = [NSMutableDictionary dictionaryWithCapacity:3];
     enumerator = [[obj valueForKey:@"toTelephone"] objectEnumerator];
 
-    while ((o = [enumerator nextObject])) {
+    while ((o = [enumerator nextObject]) != nil) {
       [tels setObject:[o valueForKey:@"telephoneId"]
              forKey:[o valueForKey:@"type"]];
     }
@@ -129,7 +152,7 @@
     NSMutableDictionary *dict;
     id                  t;
 
-    dict     = [NSMutableDictionary dictionaryWithCapacity:8];
+    dict = [NSMutableDictionary dictionaryWithCapacity:8];
     if ((t = [_obj valueForKey:type])) {
       [dict setObject:t forKey:@"number"];
       [dict setObject:type forKey:@"type"];
@@ -140,8 +163,7 @@
   }
   
   enumerator = [AddrType objectEnumerator];
-
-  while ((type = [enumerator nextObject])) {
+  while ((type = [enumerator nextObject]) != nil) {
     NSEnumerator        *attrEnum;
     id                  attr;
     NSMutableDictionary *dict;
