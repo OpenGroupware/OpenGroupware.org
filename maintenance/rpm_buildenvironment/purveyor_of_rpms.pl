07002c473d9103ec4272dfb08b70c30740c25740
Frank Reppin <frank@opengroupware.org>
2005-08-26 12:52:33 +0000

    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@1230 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- maintenance/rpm_buildenvironment/purveyor_of_rpms.pl
+++ maintenance/rpm_buildenvironment/purveyor_of_rpms.pl
@@ -138,17 +138,19 @@ sub build_rpm {
       $use_sope = $tmp_spec_line if ($tmp_spec_line =~ s/^#UseSOPE:\s+//g);
     }
     print "[RPMBUILD]          - didn't found UseSOPE hint in specfile.\n" and exit 1 if (($verbose eq "yes") and (!$use_sope));
-    print "[RPMBUILD]          - building $package using: SOPE release  $use_sope\n" if ($verbose eq "yes");
-    print "[RPMBUILD]          - getting MD5_INDEX for $use_sope prior installation...\n" if ($verbose eq "yes");
-    @t_sope = `wget -q --proxy=off -O - http://$dl_host/releases/unstable/$use_sope/$host_i_runon/MD5_INDEX` or die "I DIE: couldn't fetch MD5_INDEX (http://$dl_host/releases/unstable/$use_sope/$host_i_runon/MD5_INDEX)\n";
+    print "[RPMBUILD]          - building $package using: SOPE $use_sope\n" if ($verbose eq "yes");
+    print "[RPMBUILD]          - getting MD5_INDEX for SOPE $use_sope prior installation...\n" if ($verbose eq "yes");
+    @t_sope = `wget -q --proxy=off -O - http://$dl_host/releases/unstable/$use_sope/$host_i_runon/MD5_INDEX` or die "I DIE: couldn't fetch MD5_INDEX (http://$dl_host/releases/unstable/$use_sope/$host_i_runon/MD5_INDEX)\n" if($use_sope !~ m/^trunk/);
+    @t_sope = `wget -q --proxy=off -O - http://$dl_host/nightly/packages/$host_i_runon/$use_sope/LATESTVERSION` or die "I DIE: couldn't fetch LATESTVERSION (http://$dl_host/nightly/packages/$host_i_runon/$use_sope/LATESTVERSION)\n" if($use_sope =~ m/^trunk/);
     #parse through MD5_INDEX and generate a list of all sope RPMS... additionally download 'em into install_tmp/
     foreach $line (@t_sope) {
       chomp $line;
-      next unless($line =~ m/\.rpm$/i);
+      next unless($line =~ m/^sope.*\.rpm$/i);
       $line =~ s/^.*\s+//g;
       $sope_rpm = $line;
       print "[RPMBUILD]          - loading $sope_rpm into install_tmp/" if ($verbose eq "yes");
-      $rc = system("wget -q --proxy=off -O $ENV{HOME}/install_tmp/$sope_rpm http://$dl_host/releases/unstable/$use_sope/$host_i_runon/$sope_rpm");
+      $rc = system("wget -q --proxy=off -O $ENV{HOME}/install_tmp/$sope_rpm http://$dl_host/releases/unstable/$use_sope/$host_i_runon/$sope_rpm") if($use_sope !~ m/^trunk/);
+      $rc = system("wget -q --proxy=off -O $ENV{HOME}/install_tmp/$sope_rpm http://$dl_host/nightly/packages/$host_i_runon/$use_sope/$sope_rpm") if($use_sope =~ m/^trunk/);
       print " ...success!\n" if($rc == 0);
       print "\nFATAL: system call (wget) returned $rc whilst downloading $sope_rpm into install_tmp/\n" and exit 1 unless($rc == 0);
       push(@sope, $sope_rpm);
