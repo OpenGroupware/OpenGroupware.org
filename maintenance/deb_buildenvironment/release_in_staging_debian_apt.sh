af2cf5dd52b9ddb7bcae91ec45551ddfb892886e
Frank Reppin <frank@opengroupware.org>
2005-10-07 16:39:36 +0000

    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@1406 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- maintenance/deb_buildenvironment/release_in_staging_debian_apt.sh
+++ maintenance/deb_buildenvironment/release_in_staging_debian_apt.sh
@@ -25,15 +25,14 @@ fi
 if [ "x${RELEASE}" = "x" ]; then
   echo -en "No releaes given...\n"
   echo -en "This must be the basename of the directory in:\n"
-  #echo -en "/var/virtual_hosts/download/nightly/packages/debian/dists/${FLAVOUR}/releases/binary-i386/\n"
-  echo -en "/var/virtual_hosts/download/releases/unstable/*/${FLAVOUR}/binary-i386/\n"
+  echo -en "/var/virtual_hosts/download/releases/${STAGE}/*/${FLAVOUR}/binary-i386/\n"
   echo -en "and thus it can be one out of:\n"
-  POSSIBILITIES="`find /var/virtual_hosts/download/releases/unstable/* -type d -maxdepth 0 \! -iname 'apt4rpm' -exec basename {} \;`"
+  POSSIBILITIES="`find /var/virtual_hosts/download/releases/${STAGE}/* -type d -maxdepth 0 \! -iname 'apt4rpm' -exec basename {} \;`"
   echo -en "${POSSIBILITIES}\n"
   exit 1
 fi
 
-if [ ! -d "/var/virtual_hosts/download/releases/unstable/${RELEASE}/${FLAVOUR}" ]; then
+if [ ! -d "/var/virtual_hosts/download/releases/${STAGE}/${RELEASE}/${FLAVOUR}" ]; then
   echo -en "directory doesn't even exist... I quit.\n"
   exit 1
 fi
@@ -41,27 +40,28 @@ fi
 #exit 0
 
 source ${HOME}/sign_rpm_passphrase.secret
-rm -f /var/virtual_hosts/download/releases/unstable/${RELEASE}/${FLAVOUR}/binary-i386/Packages*
-rm -f /var/virtual_hosts/download/releases/unstable/${RELEASE}/${FLAVOUR}/binary-i386/Release*
+rm -f /var/virtual_hosts/download/releases/${STAGE}/${RELEASE}/${FLAVOUR}/binary-i386/Packages*
+rm -f /var/virtual_hosts/download/releases/${STAGE}/${RELEASE}/${FLAVOUR}/binary-i386/Release*
 
 echo "Archive: download.opengroupware.org
 Version: 1.0+opengroupware.org
 Component: ${FLAVOUR}/releases/${RELEASE}
 Origin: OpenGroupware.org
 Label: OpenGroupware.org Debian packages
-Architecture: i386" >/var/virtual_hosts/download/releases/unstable/${RELEASE}/${FLAVOUR}/binary-i386/Release
+Architecture: i386" >/var/virtual_hosts/download/releases/${STAGE}/${RELEASE}/${FLAVOUR}/binary-i386/Release
 
 /usr/bin/apt-ftparchive release \
-  /var/virtual_hosts/download/releases/unstable/${RELEASE}/${FLAVOUR}/binary-i386 \
-  >> /var/virtual_hosts/download/releases/unstable/${RELEASE}/${FLAVOUR}/binary-i386/Release
+  /var/virtual_hosts/download/releases/${STAGE}/${RELEASE}/${FLAVOUR}/binary-i386 \
+  >> /var/virtual_hosts/download/releases/${STAGE}/${RELEASE}/${FLAVOUR}/binary-i386/Release
 
 /usr/bin/expect -c \
-  "spawn /usr/bin/gpg --sign -ba /var/virtual_hosts/download/releases/unstable/${RELEASE}/${FLAVOUR}/binary-i386/Release; \
+  "spawn /usr/bin/gpg --sign -ba /var/virtual_hosts/download/releases/${STAGE}/${RELEASE}/${FLAVOUR}/binary-i386/Release; \
    expect \"Enter pass phrase:\"; \
    send -- \"${PASSPHRASE}\\r\"; \
    expect eof" >/dev/null
 
-cd /var/virtual_hosts/download/releases/unstable/
+cd /var/virtual_hosts/download/releases/${STAGE}/
 dpkg-scanpackages ${RELEASE}/${FLAVOUR}/binary-i386 /dev/null > ${RELEASE}/${FLAVOUR}/binary-i386/Packages
-cd /var/virtual_hosts/download/releases/unstable/${RELEASE}/${FLAVOUR}/binary-i386/
+cd /var/virtual_hosts/download/releases/${STAGE}/${RELEASE}/${FLAVOUR}/binary-i386/
 gzip -c Packages > Packages.gz
+
