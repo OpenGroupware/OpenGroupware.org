e664c8bc2077a7af6f490e33399bacdbb15077e1
Helge Heß <me@helgehess.eu>
2005-11-10 12:29:30 +0000

    added ChatUI
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@1448 fda58e92-02e2-0310-b1e4-b2b32738dd10

---
+++ Misc/ChatUI/ChatUIModule.m
@@ -0,0 +1,39 @@
+/*
+  Copyright (C) 2005 SKYRIX Software AG
+
+  This file is part of OpenGroupware.org.
+
+  OGo is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 2, or (at your option) any
+  later version.
+
+  OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with OGo; see the file COPYING.  If not, write to the
+  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
+/*
+ This software is a contribution of
+
+ Scinergy Consulting Ltda
+
+ Av. Rio Branco 124 sl 1002
+ Centro - Rio de Janeiro - Brazil
+ Phone +55-21-8822-8812
+ contato@scinergy.com.br
+*/
+
+
+#include <OGoFoundation/OGoModuleManager.h>
+
+@interface ChatUIModule : OGoModuleManager
+@end
+
+@implementation ChatUIModule
+@end /* ChatUIModule */
