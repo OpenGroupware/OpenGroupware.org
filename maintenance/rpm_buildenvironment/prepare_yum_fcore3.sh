ba1376feb2c8801fd2c2c10d0214ce5b3d1d438e
Frank Reppin <frank@opengroupware.org>
2005-11-17 13:39:46 +0000

    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@1461 fda58e92-02e2-0310-b1e4-b2b32738dd10

--- maintenance/rpm_buildenvironment/prepare_yum_fcore3.sh
+++ maintenance/rpm_buildenvironment/prepare_yum_fcore3.sh
@@ -8,7 +8,7 @@ rm -fr ${YUMTEMP}
 mkdir ${YUMTEMP}
 
 ssh www@download.opengroupware.org "rm -fr ${YUMREPO}/trunk/repodata/"
-ssh www@download.opengroupware.org "rm -fr ${YUMREPO}/releases/repodata/"
+ssh www@download.opengroupware.org "rm -fr ${YUMREPO}/releases/*/repodata/"
 
 echo -en "rsyncing from download.opengroupware.org...\n"
 rsync -a rsync://download.opengroupware.org/fcore3 ${YUMTEMP}
@@ -16,11 +16,13 @@ rm -fr ${YUMTEMP}/trunk/repodata
 rm -fr ${YUMTEMP}/releases/repodata
 echo -en "creating repodata/ for trunk\n"
 /usr/bin/createrepo ${YUMTEMP}/trunk 2>&1 >/dev/null
-echo -en "creating repodata/ for release\n"
-/usr/bin/createrepo ${YUMTEMP}/releases 2>&1 >/dev/null
+for RELEASE in `ls -1 ${YUMTEMP}/releases`; do
+  echo -en "creating repodata/ for release/${RELEASE}\n"
+  /usr/bin/createrepo ${YUMTEMP}/releases/${RELEASE} 2>&1 >/dev/null
+done
 
 cd ${MYHOME}/${YUMTEMP}
-tar cf - trunk/repodata --numeric-owner | ssh www@download.opengroupware.org tar xf - -C ${YUMREPO}
+tar cf - trunk/repodata | ssh www@download.opengroupware.org tar xf - -C ${YUMREPO}
 cd ${MYHOME}/${YUMTEMP}
-tar cf - releases/repodata --numeric-owner | ssh www@download.opengroupware.org tar xf - -C ${YUMREPO}
+tar cf - releases/*/repodata | ssh www@download.opengroupware.org tar xf - -C ${YUMREPO}
 
