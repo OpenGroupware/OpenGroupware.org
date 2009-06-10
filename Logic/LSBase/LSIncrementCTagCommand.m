210ec0c21ad2ced9d994a49980da593594c79e20
Adam Tauno Williams <awilliam@whitemiceconsulting.com>
2009-06-10 18:18:04 +0000

    Added the command to increment the ctags command.
    git-svn-id: http://svn.opengroupware.org/OpenGroupware.org/trunk@2255 fda58e92-02e2-0310-b1e4-b2b32738dd10

---
+++ Logic/LSBase/LSIncrementCTagCommand.m
@@ -0,0 +1,47 @@
+/*
+  Copyright (C) 2000-2005 SKYRIX Software AG
+
+  This file is part of OpenGroupware.org.
+
+  OGo is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the
+  Free Software Foundation; either version 2, or (at your option) any
+  later version.
+
+  OGo is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with OGo; see the file COPYING.  If not, write to the
+  Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+  02111-1307, USA.
+*/
+
+#import "common.h"
+#import "LSIncrementCTagCommand.h"
+
+@implementation LSIncrementCTagCommand
+
+- (void)_executeInContext:(id)_context {
+  EOAdaptorChannel    *eoChannel;
+  NSString            *sql;
+
+  [self logWithFormat:@"Incrementing ctag!"];
+  if ([[self object] isNotNull])
+  {
+    sql = [[NSString alloc] initWithFormat:@"UPDATE ctags SET ctag = (ctag + 1)" 
+                                           @"WHERE entity = '%@';", 
+                                             [[self object] entityName]];
+
+    eoChannel = [[self databaseChannel] adaptorChannel];
+    if (![eoChannel evaluateExpression:sql]) {
+      [self logWithFormat:@"failed to update ctag for entity %@", 
+                        [[self object] entityName]];
+    }
+    [sql release];
+  }
+}
+
+@end
