cf1fb28ca2eab047453e00451255a9c5ee4e0b3d
Helge Heß <devteam@zeezide.de>
2025-03-02 17:16:45 +0100

    Port model generator to Python 3
    ... hope all that is right.

--- Database/OGoModel/genmodel.py
+++ Database/OGoModel/genmodel.py
@@ -1,10 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # $Id: genmodel.py,v 1.1.1.1 2003/07/09 22:57:26 cvs Exp $
 
 from types  import *
 from sys    import argv
 import sys;
-from string import uppercase, split
+import string;
+# Alias ascii_uppercase to uppercase for Python 2 compatibility
+uppercase = string.ascii_uppercase
+# Define a split function to match Python 2 import style
+def split(s, sep=None):
+    """Stub to mimic Python 2 behavior of importing split from string."""
+    return s.split(sep)
+from functools import cmp_to_key;
+
+
 
 skyModelPath = argv[1]
 adaptorName  = argv[2]
@@ -72,17 +81,20 @@ def nameSortFunc(a, b):
 class Constants:
     pass
 
-execfile(skyModelPath, modelenv)
-
+# Python 2: execfile(skyModelPath, modelenv)
+# Python 3:
+with open(skyModelPath, "r") as file:
+    exec(file.read(), modelenv)
+    
 userTypes   = modelenv['userTypes'][adaptorName]
 adaptorInfo = modelenv['adaptorInfo'][adaptorName]
 entities    = {}
 c           = Constants()
 
 for key, value in modelenv.items():
-    if (key[:1] in uppercase) & (type(value) == DictionaryType):
+    if (key[:1] in uppercase) & (isinstance(value, dict)):
         entities[key] = value
-    if type(value) is StringType:
+    if isinstance(value, str):
         if value[:1] == '@':
             setattr(c, key, value)
 
@@ -163,9 +175,11 @@ def processEntity(name, info):
     for propertyName, propertyInfo in info.items():
         if propertyName[:1] == '@':
             continue
-        if type(propertyInfo) is not DictionaryType:
+        # Py2: if type(propertyInfo) is not DictionaryType:
+        if not isinstance(propertyInfo, dict):
             continue
-        if not propertyInfo.has_key(c.column):
+        # Py2: if not propertyInfo.has_key(c.column):
+        if c.column not in propertyInfo:
             continue
         
         flags = getKey(propertyInfo, c.flags)
@@ -180,7 +194,7 @@ def processEntity(name, info):
         propertyInfo = processAttribute(propertyName, propertyInfo)
         attributes.append(propertyInfo)
     
-    attributes.sort(nameSortFunc)
+    attributes.sort(key=cmp_to_key(nameSortFunc))
     attributes = tuple(attributes)
     
     # process relationships
@@ -188,9 +202,11 @@ def processEntity(name, info):
     for propertyName, propertyInfo in info.items():
         if propertyName[:1] == '@':
             continue
-        if type(propertyInfo) is not DictionaryType:
+        # Py2: if type(propertyInfo) is not DictionaryType:
+        if not isinstance(propertyInfo, dict):
             continue
-        if propertyInfo.has_key(c.column):
+        # Py2: if propertyInfo.has_key(c.column):
+        if c.column in propertyInfo:
             continue
         
         flags = getKey(propertyInfo, c.flags)
@@ -201,7 +217,7 @@ def processEntity(name, info):
         propertyInfo = processRelationship(propertyName, propertyInfo)
         relationships.append(propertyInfo)
     
-    relationships.sort(nameSortFunc)
+    relationships.sort(key=cmp_to_key(nameSortFunc))
     relationships = tuple(relationships)
     
     # assign
@@ -217,37 +233,38 @@ def processEntity(name, info):
 adaptorEntities = []
 
 for entityName, entityInfo in entities.items():
-    if (entityInfo.has_key('table_name')):
+    # Py2: if (entityInfo.has_key('table_name')):
+    if 'table_name' in entityInfo:
         entityName = entityInfo['table_name'];
     
     adaptorEntities.append(processEntity(entityName, entityInfo))
 
-adaptorEntities.sort(nameSortFunc)
+adaptorEntities.sort(key=cmp_to_key(nameSortFunc))
 
 # generate output
 
 def genAttribute(info):
-    print attributeHeader % info
-    if info.has_key('valueType'):
-        print "          valueType      = %(valueType)s;" % info
-    if info.has_key('allowsNull'):
-        print "          allowsNull     = %(allowsNull)s;" % info
-    if info.has_key('width'):
-        print "          width          = %(width)i;" % info
-    if info.has_key('calendarFormat'):
-        print "          calendarFormat = \"%(calendarFormat)s\";" % info
-    print attributeFooter % info
+    print(attributeHeader % info)
+    if 'valueType' in info:
+        print("          valueType      = %(valueType)s;" % info)
+    if 'allowsNull' in info:
+        print("          allowsNull     = %(allowsNull)s;" % info)
+    if 'width' in info:
+        print("          width          = %(width)i;" % info)
+    if 'calendarFormat' in info:
+        print("          calendarFormat = \"%(calendarFormat)s\";" % info)
+    print(attributeFooter % info)
 
 def genRelationship(info):
-    print relationshipHeader % info
+    print(relationshipHeader % info)
     
-    if info.has_key("isToMany"):
-        print "          isToMany       = %(isToMany)s;" % info
+    if "isToMany" in info:
+        print("          isToMany       = %(isToMany)s;" % info)
     
-    print relationshipFooter % info
+    print(relationshipFooter % info)
 
 def genEntity(info):
-    print entityHeader % info
+    print(entityHeader % info)
 
     # generate property infos
     
@@ -258,12 +275,12 @@ def genEntity(info):
         s = s[:-2] + " );"
         
         if len(s) > 78:
-            print "      attributesUsedForLocking = ("
+            print("      attributesUsedForLocking = (")
             for attribute in info['attributesUsedForLocking']:
-                print "        %s," % attribute
-            print "      );"
+                print("        %s," % attribute)
+            print("      );")
         else:
-            print s
+            print(s)
     
     if len(info['classProperties']) > 0:
         s = "      classProperties = ( "
@@ -272,42 +289,42 @@ def genEntity(info):
         s = s[:-2] + " );"
         
         if len(s) > 78:
-            print "      classProperties = ("
+            print("      classProperties = (")
             for property in info['classProperties']:
-                print "        %s," % property
-            print "      );"
+                print("        %s," % property)
+            print("      );")
         else:
-            print s
+            print(s)
     
     # generate attribute infos
     
     if len(info['attributes']):
-        print attributesHeader % info
+        print(attributesHeader % info)
         
         for attribute in info['attributes']:
             genAttribute(attribute)
         
-        print attributesFooter % info
+        print(attributesFooter % info)
     
     # generate relationship infos
     
     if len(info['relationships']):
-        print relationshipsHeader % info
+        print(relationshipsHeader % info)
         
         for relship in info['relationships']:
             genRelationship(relship)
         
-        print relationshipsFooter % info
+        print(relationshipsFooter % info)
     
-    print entityFooter % info
+    print(entityFooter % info)
 
 # print EOModel header
 
-print header % adaptorInfo
+print(header % adaptorInfo)
 
 for entityInfo in adaptorEntities:
     genEntity(entityInfo)
 
 # print EOModel footer
 
-print footer
+print(footer)
